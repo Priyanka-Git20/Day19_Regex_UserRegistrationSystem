9e4aafc2ce75ddf90a40df3a1b915ad914f301f6
priyanka <priyanka.chavan1408@gmail.com>
2022-02-24 21:17:16 +0530

    ADDED:Welcome message on the master branch.

---
+++ UserRegistrationRegex.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.regex;
+
+public class UserRegistrationRegex {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the user registration system.");
+    }
+}
